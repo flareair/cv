efbac6f761096c7015352cb9169647e2c5493f46
flareair <flareair0@gmail.com>
2016-07-19 16:25:09 +0300

    Add eng cv to compilation process

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const fs = require("fs");
 fs.createReadStream("./cv_rus.md")
     .pipe(markdownpdf())
     .pipe(fs.createWriteStream("./cv_rus.pdf"));
+
+
+fs.createReadStream("./cv_eng.md")
+    .pipe(markdownpdf())
+    .pipe(fs.createWriteStream("./cv_eng.pdf"));
